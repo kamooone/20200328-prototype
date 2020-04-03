625c68d3699cd82e9858ccd80a4a73278363690c
kamooone <61107134+kamooone@users.noreply.github.com>
2020-04-03 14:49:53 +0900

    ポーズ画面追加
    Pキーでポーズ画面

--- Assets/PROTO/Script/PlayerMove.cs
+++ Assets/PROTO/Script/PlayerMove.cs
@@ -33,6 +33,12 @@ public class PlayerMove : MonoBehaviour//プレイヤー移動処理(現在は2D
     // Update is called once per frame
     void Update()
     {
+        //ポーズ画面になるとUpdate以外の処理も止める
+        if (Mathf.Approximately(Time.timeScale, 0f))
+        {
+            return;
+        }
+
         /*画像切り替え処理*/
         if (Input.GetKey("left"))//左に移動
         {
