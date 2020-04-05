12204bfb02fd4ca65c572b89cf8bec31d1456271
yutyo1231 <62589869+yutyo1231@users.noreply.github.com>
2020-04-06 03:56:28 +0900

    水の水位変更(UI)
    現在はUIでいいとのことなのでUIで処理を行っています。(一応オブジェクトの方も作ってはいますが間に合うかわからないため先にUIの方を上げます)
    
    ↑ボタン↓ボタンを押していただくと一定分水位が上がります。
    水位の量はyという変数で変更することができます。

---
+++ Assets/PROTO/Script/WaterUPUI.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterUPUI : MonoBehaviour//水を上げる処理
+{
+    public GameObject gameobj;
+    public float y;//上昇させる値
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameobj.transform.localScale.y > 0.5)//倍率が0.5以上になったら0.05にする
+        {
+            gameobj.transform.localScale = new Vector3(gameobj.transform.localScale.x, 0.5f, gameobj.transform.localScale.z);
+        }
+    }
+
+    //ボタンをクリックした時の処理
+    public void OnClick()
+    {
+        gameobj.transform.localScale += new Vector3(0, y, 0);
+    }
+
+}
