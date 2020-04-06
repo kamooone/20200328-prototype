8744a18d2a467d66f3050548d8564475850225d9
KSHashimoto <62603960+KSHashimoto@users.noreply.github.com>
2020-04-07 00:06:03 +0900

    0406足音実装Ver.02

--- Assets/PROTO/Script/PlayerSound.cs
+++ Assets/PROTO/Script/PlayerSound.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//足音スクリプト
 public class PlayerSound : MonoBehaviour
 {
     // Start is called before the first frame update
@@ -11,31 +12,71 @@ public class PlayerSound : MonoBehaviour
     public AudioClip sound2;
     AudioSource audiosource;
 
+    //判定用変数
+    bool b_left = false;
+    bool b_right = false;
+
+    //waterそのものが入る変数
+    GameObject water;
+
+    //VariableManagerスクリプトが入る変数
+    VariableManager script; 
+
     void Start()
     {
         //コンポーネントを取得
         audiosource = GetComponent<AudioSource>();
         audiosource.clip = sound1;
-    }
 
-    void OnCollisionStay(Collision collision)
-    {
-        Debug.Log(collision.gameObject.name); // ログを表示する
+        water = GameObject.Find("Water"); //Waterをオブジェクトの名前から取得して変数に格納する
     }
 
     // Update is called once per frame
     void Update()
     {
-        //AキーまたはDキーが押されたらSEを再生する
-        if (Input.GetKeyDown("left")|| Input.GetKeyDown("right"))
+        script = water.GetComponent<VariableManager>(); //Waterの中にあるVariableManagerを取得して変数に格納する
+        if (script.soundNo == 2)
         {
-            audiosource.Play();
+            audiosource.clip = sound2;
+        }else
+        {
+            audiosource.clip = sound1;
+        }
+
+        //AキーまたはDキーが押されたらSEを再生する
+        if (Input.GetKeyDown("left"))
+        {
+            audiosource.Play(); //再生
+            b_left = true;      //左キーが押されたtrueに
+            
+        }
+
+        if (Input.GetKeyDown("right"))
+        {
+            audiosource.Play(); //再生
+            b_right = true;     //右キーが押されたらtrueに
+           
         }
 
         //AキーまたはDキーから手が離れたらSEを停止させる
-        if (Input.GetKeyUp("left")|| Input.GetKeyUp("right"))
+        if (Input.GetKeyUp("left"))
         {
-            audiosource.Stop();
+            b_left = false;
+            audiosource.Stop();     //停止
+            if (b_right == true)    //左キーが離れた際に右キーが押されていたら
+            {
+                audiosource.Play(); //再生
+            }
+        }
+
+        if(Input.GetKeyUp("right"))
+        {
+            b_right = false;
+            audiosource.Stop();     //停止
+            if (b_left == true)     //右キーが離れた際に左キーが押されていたら
+            {
+                audiosource.Play(); //再生
+            }
         }
     }
 }
---
+++ Assets/PROTO/Script/VariableManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//変数管理スクリプト
+public class VariableManager : MonoBehaviour
+{
+    public int soundNo = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //重なり判定　重なっている間
+    void OnTriggerStay(Collider collider)
+    {
+        //重なっているのが名前"player"なら
+        if (collider.gameObject.name == "player")
+        {
+            soundNo = 2;
+            //Debug.Log(collider.gameObject.name); // ログを表示する
+        }else
+        {
+            soundNo = 1;
+        }
+    }
+}
