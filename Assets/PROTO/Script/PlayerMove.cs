c20e6f8e6cb0a4a8cc8f16560a9c760f383d9008
kamooone <61107134+kamooone@users.noreply.github.com>
2020-04-08 00:35:11 +0900

    水上げ下げオブジェクト
    20200408

--- Assets/PROTO/Script/PlayerMove.cs
+++ Assets/PROTO/Script/PlayerMove.cs
@@ -5,6 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class PlayerMove : MonoBehaviour
 {
+    GameObject W_Machine1;
+    GameObject W_Machine2;
+    GameObject W_Machine3;
+    GameObject W_Machine4;
+    GameObject W_Machine5;
 
     public float speed = 10.0f;
 
@@ -19,6 +24,48 @@ public class PlayerMove : MonoBehaviour
     public bool UIUp_Flag = false;
     public bool UIDown_Flag = false;
 
+    //水増し機1
+    float WaterHight1 = 0.0f;
+    bool WaterUp1_Flag = false;
+    bool WaterUp1 = false;
+
+    bool WaterDown1_Flag = true;
+    bool WaterDown1 = false;
+
+    //水増し機2
+    float WaterHight2 = 0.0f;
+    bool WaterUp2_Flag = false;
+    bool WaterUp2 = false;
+
+    bool WaterDown2_Flag = true;
+    bool WaterDown2 = false;
+
+    //水増し機3
+    float WaterHight3 = 0.0f;
+    bool WaterUp3_Flag = false;
+    bool WaterUp3 = false;
+
+    bool WaterDown3_Flag = true;
+    bool WaterDown3 = false;
+
+    //水増し機4
+    float WaterHight4 = 0.0f;
+    bool WaterUp4_Flag = false;
+    bool WaterUp4 = false;
+
+    bool WaterDown4_Flag = true;
+    bool WaterDown4 = false;
+
+    //水増し機3
+    float WaterHight5 = 0.0f;
+    bool WaterUp5_Flag = false;
+    bool WaterUp5 = false;
+
+    bool WaterDown5_Flag = true;
+    bool WaterDown5 = false;
+
+
+
     //プレイヤーの左向き右向きを表す
     int PlayerDirection = 1;
 
@@ -28,7 +75,12 @@ public class PlayerMove : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        // Watermachineコンポーネントの取得
+        W_Machine1 = GameObject.Find("Water/WaterProNighttime1");
+        W_Machine2 = GameObject.Find("Water/WaterProNighttime2");
+        W_Machine3 = GameObject.Find("Water/WaterProNighttime3");
+        W_Machine4 = GameObject.Find("Water/WaterProNighttime4");
+        W_Machine5 = GameObject.Find("Water/WaterProNighttime5");
     }
 
     // Update is called once per frame
@@ -77,6 +129,71 @@ public class PlayerMove : MonoBehaviour
             //下の階層へ
             ClassDown();
         }
+
+
+        //一階の水増し機
+        if (WaterUp1 == true)
+        {
+            //一階の水増し
+            WaterGain1();
+        }
+        if (WaterDown1 == true)
+        {
+            //一階の水減
+            WaterLoss1();
+        }
+
+
+        //二階の水増し機
+        if (WaterUp2 == true)
+        {
+            //二階の水増し
+            WaterGain2();
+        }
+        if (WaterDown2 == true)
+        {
+            //二階の水減
+            WaterLoss2();
+        }
+
+
+        //三階の水増し機
+        if (WaterUp3 == true)
+        {
+            //三階の水増し
+            WaterGain3();
+        }
+        if (WaterDown3 == true)
+        {
+            //三階の水減
+            WaterLoss3();
+        }
+
+
+        //四階の水増し機
+        if (WaterUp4 == true)
+        {
+            //三階の水増し
+            WaterGain4();
+        }
+        if (WaterDown4 == true)
+        {
+            //三階の水減
+            WaterLoss4();
+        }
+
+
+        //五階の水増し機
+        if (WaterUp5 == true)
+        {
+            //三階の水増し
+            WaterGain5();
+        }
+        if (WaterDown5 == true)
+        {
+            //三階の水減
+            WaterLoss5();
+        }
     }
     
 
@@ -100,6 +217,126 @@ public class PlayerMove : MonoBehaviour
             if (Input.GetKey("down"))
             {
                 ClassDown_Flag = true;
+            }
+        }
+
+        //水増し機１との判定
+        if (collision.gameObject.tag == "Water1")
+        {
+            if (WaterHight1 == 0.0f || WaterHight1 == -0.1f)
+            {
+                WaterUp1_Flag = true;
+                UIUp_Flag = true;
+            }
+            if (WaterHight1 == 0.1f)
+            {
+                WaterDown1_Flag = true;
+                UIDown_Flag = true;
+            }
+
+            if (Input.GetKey("down") && WaterDown1_Flag == true)
+            {
+                WaterDown1 = true;
+            }
+            if (Input.GetKey("up") && WaterUp1_Flag == true)
+            {
+                WaterUp1 = true;
+            }
+        }
+
+        //水増し機2との判定
+        if (collision.gameObject.tag == "Water2")
+        {
+            if (WaterHight2 == 0.0f || WaterHight2 == -0.1f)
+            {
+                WaterUp2_Flag = true;
+                UIUp_Flag = true;
+            }
+            if (WaterHight2 == 0.1f)
+            {
+                WaterDown2_Flag = true;
+                UIDown_Flag = true;
+            }
+
+            if (Input.GetKey("down") && WaterDown2_Flag == true)
+            {
+                WaterDown2 = true;
+            }
+            if (Input.GetKey("up") && WaterUp2_Flag == true)
+            {
+                WaterUp2 = true;
+            }
+        }
+
+        //水増し機3との判定
+        if (collision.gameObject.tag == "Water3")
+        {
+            if (WaterHight3 == 0.0f || WaterHight3 == -0.1f)
+            {
+                WaterUp3_Flag = true;
+                UIUp_Flag = true;
+            }
+            if (WaterHight3 == 0.1f)
+            {
+                WaterDown3_Flag = true;
+                UIDown_Flag = true;
+            }
+
+            if (Input.GetKey("down") && WaterDown3_Flag == true)
+            {
+                WaterDown3 = true;
+            }
+            if (Input.GetKey("up") && WaterUp3_Flag == true)
+            {
+                WaterUp3 = true;
+            }
+        }
+
+        //水増し機4との判定
+        if (collision.gameObject.tag == "Water4")
+        {
+            if (WaterHight4 == 0.0f || WaterHight4 == -0.1f)
+            {
+                WaterUp4_Flag = true;
+                UIUp_Flag = true;
+            }
+            if (WaterHight4 == 0.1f)
+            {
+                WaterDown4_Flag = true;
+                UIDown_Flag = true;
+            }
+
+            if (Input.GetKey("down") && WaterDown4_Flag == true)
+            {
+                WaterDown4 = true;
+            }
+            if (Input.GetKey("up") && WaterUp4_Flag == true)
+            {
+                WaterUp4 = true;
+            }
+        }
+
+        //水増し機5との判定
+        if (collision.gameObject.tag == "Water5")
+        {
+            if (WaterHight5 == 0.0f || WaterHight5 == -0.1f)
+            {
+                WaterUp5_Flag = true;
+                UIUp_Flag = true;
+            }
+            if (WaterHight5 == 0.1f)
+            {
+                WaterDown5_Flag = true;
+                UIDown_Flag = true;
+            }
+
+            if (Input.GetKey("down") && WaterDown5_Flag == true)
+            {
+                WaterDown5 = true;
+            }
+            if (Input.GetKey("up") && WaterUp5_Flag == true)
+            {
+                WaterUp5 = true;
             }
         }
     }
@@ -126,7 +363,7 @@ public class PlayerMove : MonoBehaviour
 
     void ClassUp()
     {
-        Debug.Log("アップ");
+        Debug.Log("階層アップ");
 
         transform.position = new Vector3(transform.position.x, transform.position.y + up, transform.position.z);
 
@@ -142,7 +379,7 @@ public class PlayerMove : MonoBehaviour
 
     void ClassDown()
     {
-        Debug.Log("ダウン");
+        Debug.Log("階層ダウン");
 
         transform.position = new Vector3(transform.position.x, transform.position.y - Down, transform.position.z);
 
@@ -154,6 +391,167 @@ public class PlayerMove : MonoBehaviour
             ClassDown_Flag = false;
             UIDown_Flag = false;
         }
+    }
+
+
+    //一階の水増し機
+    void WaterGain1()
+    {
+        Debug.Log("水アップ");
+
+        WaterHight1 = 0.1f;
+        
+        W_Machine1.transform.position = new Vector3(W_Machine1.transform.position.x, W_Machine1.transform.position.y + WaterHight1, W_Machine1.transform.position.z);
+
+        WaterUp1_Flag = false;
+
+        WaterUp1 = false;
+
+        UIUp_Flag = false;
+    }
+
+    void WaterLoss1()
+    {
+        Debug.Log("水ダウン");
+
+        WaterHight1 = -0.1f;
+
+        W_Machine1.transform.position = new Vector3(W_Machine1.transform.position.x, W_Machine1.transform.position.y + WaterHight1, W_Machine1.transform.position.z);
+
+        WaterDown1_Flag = false;
+
+        WaterDown1 = false;
+
+        UIDown_Flag = false;
+    }
+
+
+
+    //二階の水増し機
+    void WaterGain2()
+    {
+        Debug.Log("水アップ");
+
+        WaterHight2 = 0.1f;
+
+        W_Machine2.transform.position = new Vector3(W_Machine2.transform.position.x, W_Machine2.transform.position.y + WaterHight2, W_Machine2.transform.position.z);
+
+        WaterUp2_Flag = false;
+
+        WaterUp2 = false;
+
+        UIUp_Flag = false;
+    }
+
+    void WaterLoss2()
+    {
+        Debug.Log("水ダウン");
+
+        WaterHight2 = -0.1f;
+
+        W_Machine2.transform.position = new Vector3(W_Machine2.transform.position.x, W_Machine2.transform.position.y + WaterHight2, W_Machine2.transform.position.z);
+
+        WaterDown2_Flag = false;
+
+        WaterDown2 = false;
+
+        UIDown_Flag = false;
+    }
+
+
+    //三階の水増し機
+    void WaterGain3()
+    {
+        Debug.Log("水アップ");
+
+        WaterHight3 = 0.1f;
+
+        W_Machine3.transform.position = new Vector3(W_Machine3.transform.position.x, W_Machine3.transform.position.y + WaterHight3, W_Machine3.transform.position.z);
+
+        WaterUp3_Flag = false;
+
+        WaterUp3 = false;
+
+        UIUp_Flag = false;
+    }
+
+    void WaterLoss3()
+    {
+        Debug.Log("水ダウン");
+
+        WaterHight3 = -0.1f;
+
+        W_Machine3.transform.position = new Vector3(W_Machine3.transform.position.x, W_Machine3.transform.position.y + WaterHight3, W_Machine3.transform.position.z);
+
+        WaterDown3_Flag = false;
+
+        WaterDown3 = false;
+
+        UIDown_Flag = false;
+    }
+
+
+    //四階の水増し機
+    void WaterGain4()
+    {
+        Debug.Log("水アップ");
+
+        WaterHight4 = 0.1f;
+
+        W_Machine4.transform.position = new Vector3(W_Machine4.transform.position.x, W_Machine4.transform.position.y + WaterHight4, W_Machine4.transform.position.z);
+
+        WaterUp4_Flag = false;
+
+        WaterUp4 = false;
+
+        UIUp_Flag = false;
+    }
+
+    void WaterLoss4()
+    {
+        Debug.Log("水ダウン");
+
+        WaterHight4 = -0.1f;
+
+        W_Machine4.transform.position = new Vector3(W_Machine4.transform.position.x, W_Machine4.transform.position.y + WaterHight4, W_Machine4.transform.position.z);
+
+        WaterDown4_Flag = false;
+
+        WaterDown4 = false;
+
+        UIDown_Flag = false;
+    }
+
+
+    //五階の水増し機
+    void WaterGain5()
+    {
+        Debug.Log("水アップ");
+
+        WaterHight5 = 0.1f;
+
+        W_Machine5.transform.position = new Vector3(W_Machine5.transform.position.x, W_Machine5.transform.position.y + WaterHight5, W_Machine5.transform.position.z);
+
+        WaterUp5_Flag = false;
+
+        WaterUp5 = false;
+
+        UIUp_Flag = false;
+    }
+
+    void WaterLoss5()
+    {
+        Debug.Log("水ダウン");
+
+        WaterHight5 = -0.1f;
+
+        W_Machine5.transform.position = new Vector3(W_Machine5.transform.position.x, W_Machine5.transform.position.y + WaterHight5, W_Machine5.transform.position.z);
+
+        WaterDown5_Flag = false;
+
+        WaterDown5 = false;
+
+        UIDown_Flag = false;
     }
 }
 
