30a7a6ade017857ae3858c2bd2cde4e0baefc048
akatuki35 <akatuki35@users.noreply.github.com>
2020-04-07 12:16:32 +0900

    test
    fujioka

---
+++ Assets/PROTO/Script/CountKey.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountKey : MonoBehaviour
+{
+    public int countkey = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<Text>().text = "鍵" + countkey.ToString() + "個";
+    }
+}
---
+++ Assets/PROTO/Script/KeyCube.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyCube : MonoBehaviour
+{
+    public CountKey countkey;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            countkey.GetComponent<CountKey>().countkey++;
+            Destroy(this.gameObject);
+        }
+    }
+}
