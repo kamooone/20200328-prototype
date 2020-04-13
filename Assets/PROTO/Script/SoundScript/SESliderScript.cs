8374d46842491fb6356d7dcfbfaa057de95eae7f
kamooone <61107134+kamooone@users.noreply.github.com>
2020-04-13 19:48:26 +0900

    ゲーム内でのサウンド設定実装(仮)

---
+++ Assets/PROTO/Script/SoundScript/SESliderScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class SESliderScript : MonoBehaviour
+{
+    Slider slider;
+
+    //BGM音量
+    static float volume = 0.5f;
+
+    void Start()
+    {
+        slider = this.GetComponent<Slider>();
+        //バックアップした音量取得
+        slider.value = PlayerPrefs.GetFloat("SEVOLUME", volume);
+    }
+
+    void Update()
+    {
+        //現在の音量をバックアップ
+        volume = slider.value;
+    }
+
+    //sliderの所にこれを起動しています
+    public void masterVol(Slider slider)
+    {
+        //現在の音量をバックアップ
+        PlayerPrefs.SetFloat("SEVOLUME", volume);
+    }
+
+}
---
+++ Assets/PROTO/Script/SoundScript/VolumeController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VolumeController : MonoBehaviour
+{
+
+    AudioSource m_AudioSource;
+
+    public Slider m_Slider;
+    
+    void Start()
+    {
+        m_AudioSource = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        m_AudioSource.volume = m_Slider.GetComponent<Slider>().normalizedValue;
+    }
+}
