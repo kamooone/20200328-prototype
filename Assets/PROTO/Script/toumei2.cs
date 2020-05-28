f72534e4e71fa1333b57e7dd33ee9136bd274893
nakashima0923 <49778600+nakashima0923@users.noreply.github.com>
2020-05-28 17:09:48 +0900

    シェーダーの値変更スクリプト(toumei2)の追加

---
+++ Assets/PROTO/Script/toumei2.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class toumei2 : MonoBehaviour
+{
+    private Material material;
+    public float F = 1.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.material = this.GetComponent<Renderer>().material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //F = F - 10.0f;
+        //if (F <= 0)//透明になったら戻す
+        //{
+        //    F = 100.0f;
+        //}
+        Color color = new Color(1.0f, 1.0f,1.0f, F);
+
+        material.SetColor("_HorizonColor", color);
+    }
+}
